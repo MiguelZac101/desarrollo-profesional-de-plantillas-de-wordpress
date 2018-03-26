a71855928ab9c37e30b3c5a409de60904638c351
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-03-26 15:12:14 -0500

    74. Trabajando con el footer

--- wp-content/themes/lapizzeria/footer.php
+++ wp-content/themes/lapizzeria/footer.php
@@ -1,4 +1,17 @@
-    
+<footer>
+    <?php
+        $args = array(
+            'theme_location' => 'header-menu',
+            'container' => 'nav',
+            'after' => '<span class="separador"> | </span>'
+        );
+        wp_nav_menu($args);
+    ?>
+    <div class="ubicacion">
+        <p>8179 Bay Avenue Mountan View, CA 94043</p>
+        <p>Teléfono: +1-92-456-7890</p>
+    </div>
+</footer>
         <?php wp_footer(); ?>
     </body>
 </html>
