c4ec0abf098d479d04e04753023fefc192523c94
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-03-28 12:22:32 -0500

    85. Realizando una consulta personalizada con WordPress

--- wp-content/themes/lapizzeria/page-especialidades.php
+++ wp-content/themes/lapizzeria/page-especialidades.php
@@ -1,22 +1,46 @@
 <?php get_header(); ?>
 
-    <?php while(have_posts()): the_post(); ?>
+<?php while (have_posts()): the_post(); ?>
 
     <?php // the_post_thumbnail();?>
 
-<div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url();?>);">
+    <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
         <div class="contenido-hero">
             <div class="texto-hero">
-                <?php the_title('<h1>','</h1>');?>
+                <?php the_title('<h1>', '</h1>'); ?>
             </div>
         </div>
     </div>
     <div class="principal contenedor">
         <main class="text-centrado contenido-paginas">
-            <?php the_content();?>
+            <?php the_content(); ?>
         </main>
     </div>
-        
-    <?php endwhile; ?>
+
+<?php endwhile; ?>
+
+<div class="nuestras-especialidades contenedor">
+    <h3 class="">Pizzas</h3>
+    <?php
+    $args = array(
+        'post_type' => 'especialidades',
+        'posts_per_page' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'category_name' => 'pizzas'
+    );
+    $pizzas = new WP_Query($args);
+    while ($pizzas->have_posts()): $pizzas->the_post();
+        ?>
+        <ul>
+            <li>
+                <?php the_title(); ?>
+            </li>
+        </ul>
+        <?php
+    endwhile;
+    wp_reset_postdata();
+    ?>
+</div>
 
 <?php get_footer(); ?>
