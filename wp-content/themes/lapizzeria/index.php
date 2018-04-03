2cffb8469f93639df8ad72534cfc09c00ad3d387
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-04-03 17:43:53 -0500

    91. Imprimiendo la imagen destacada en el blog

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,21 +1,30 @@
 <?php get_header(); ?>
+<?php 
+$pagina_blog = get_option('page_for_posts'); //obtienen la pagina actual
+$id_imagen = get_post_thumbnail_id($pagina_blog);
+$imagen = wp_get_attachment_image_src($id_imagen,'full');//(id_imagen,tamaño)
+?>  
 
-    <?php while(have_posts()): the_post(); ?>
-
-    <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url();?>);">
+    <div class="hero" style="background-image: url(<?php echo $imagen['0'];?>);">
         <div class="contenido-hero">
             <div class="texto-hero">
-                <?php the_title('<h1>','</h1>');?>
+                <h1>
+                    <?php echo get_the_title($pagina_blog) ;?>
+                </h1>                
             </div>
         </div>
     </div>
     <div class="principal contenedor">
         <main class="text-centrado contenido-paginas">
-            <?php // the_content();?>
+            <?php while(have_posts()): the_post(); ?>
+            <article class="entrada-blog">
+                <?php the_title(); ?>
+            </article>            
+            <?php endwhile; ?>
         </main>
     </div>
         
-    <?php endwhile; ?>
+    
     
 <?php get_footer(); ?>
     
