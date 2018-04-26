cf52cc0041c9053102b96ed19b2483c5a30146f9
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-04-26 12:38:52 -0500

    118. Insertando los datos del formulario.

--- wp-content/themes/lapizzeria/inc/reservaciones.php
+++ wp-content/themes/lapizzeria/inc/reservaciones.php
@@ -2,6 +2,9 @@
 
 //https://codex.wordpress.org/Validating_Sanitizing_and_Escaping_User_Data
 function lapizzeria_guardar() {
+    global $wpdb;
+    $tabla = $wpdb->prefix . "reservaciones";
+
     if (isset($_POST['enviar']) && $_POST['oculto'] == 1):
 
         $nombre = sanitize_text_field($_POST['nombre']);
@@ -9,6 +12,27 @@ function lapizzeria_guardar() {
         $correo = sanitize_text_field($_POST['correo']);
         $telefono = sanitize_text_field($_POST['telefono']);
         $mensaje = sanitize_text_field($_POST['mensaje']);
+
+        //https://codex.wordpress.org/Class_Reference/wpdb
+        $datos = array(
+            'nombre' => $nombre,
+            'fecha' => $fecha,
+            'correo' => $correo,
+            'telefono' => $telefono,
+            'mensaje' => $mensaje
+        );
+        $formato = array(
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+        );
+//    '%d' -> cantidad
+//    '%s' -> string
+//    '%f' -> flotante
+
+        $wpdb->insert($tabla, $datos, $formato);
 
     endif;
 }
