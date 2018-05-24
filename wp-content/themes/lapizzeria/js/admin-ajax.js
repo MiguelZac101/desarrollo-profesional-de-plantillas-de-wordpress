80a8db3022ef673e427f88de68c74ef61d5c8241
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-05-24 11:31:14 -0500

    166. Agregando una alerta de confirmacion para borrar la reservación.

--- wp-content/themes/lapizzeria/js/admin-ajax.js
+++ wp-content/themes/lapizzeria/js/admin-ajax.js
@@ -6,27 +6,43 @@ $(document).ready(function () {
     $('.borrar_registro').on('click', function (e) {
         e.preventDefault();
         var id = $(this).attr('data-reservaciones');
+        
+        swal({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if(result.value) {
 
-        $.ajax({
-            type: 'post',
-            data: {
-                action: 'lapizzeria_eliminar',
-                id: id,
-                tipo: 'eliminar'
-            },
-            url: url_eliminar.ajaxurl,
-            success: function (data) {
-                var resultado = JSON.parse(data);
-                if (resultado.respuesta == 1) {
-                    jQuery("[data-reservaciones='" + resultado.id + "']").parent().parent().remove();
-                    swal(
-                        'Eliminado!',
-                        'La reservacion se ha eliminado!',
-                        'success'
-                        );
-                }
+               $.ajax({
+                      type: 'post',
+                      data: {
+                          action: 'lapizzeria_eliminar',
+                          id: id,
+                          tipo: 'eliminar'
+                      },
+                      url: url_eliminar.ajaxurl,
+                      success: function (data) {
+                          var resultado = JSON.parse(data);
+                          if (resultado.respuesta == 1) {
+                              jQuery("[data-reservaciones='" + resultado.id + "']").parent().parent().remove();
+                              swal(
+                                  'Eliminado!',
+                                  'La reservacion se ha eliminado!',
+                                  'success'
+                                  );
+                          }
+                      }
+                  });
+
             }
-        });
+         });        
+       
 
     });
 });
